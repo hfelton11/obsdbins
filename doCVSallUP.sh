658280b5639500a6e45bc90b4c81b24322d909c0
hfelton <hfelton@zid89.hfelton.local>
2019-07-12 02:39:04 -0700

    mv cvs-up to git

--- doCVSallUP.sh
+++ doCVSallUP.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 echo "Starting doCVSallUP.sh..."
-cd /usr
-cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 src
+#cd /usr
+#cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 src
+#echo "...src-done start-xenocara..."
+#cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 xenocara
+#echo "...xenocara-done start-ports..."
+#cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 ports
+#echo "...ports-done start-www..."
+## had to use git here...
+##cd /usr/www/GIT
+##git clone https://github.com/openbsd/www.git
+##cd www ;git branch hjflocal ;git checkout hjflocal
+#cd /usr/www/GIT/www
+##git checkout master ;git pull ;git checkout hjflocal
+#git pull origin master
+#
+# choosing to do all of these via git, since faster/easier than cvs...
+cd /xtra/src/GIT/src
+git pull origin master
 echo "...src-done start-xenocara..."
-cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 xenocara
+cd /xtra/xenocara/GIT/xenocara
+git pull origin master
 echo "...xenocara-done start-ports..."
-cvs -qd anoncvs@anoncvs1.usa.openbsd.org:/cvs up -Pd -rOPENBSD_6_5 ports
+cd /xtra/ports/GIT/ports
+git pull origin master
 echo "...ports-done start-www..."
-# had to use git here...
-#cd /usr/www/GIT
-#git clone https://github.com/openbsd/www.git
-#cd www ;git branch hjflocal ;git checkout hjflocal
-cd /usr/www/GIT/www
-#git checkout master ;git pull ;git checkout hjflocal
+cd /xtra/www/GIT/www
 git pull origin master
 echo "... doCVSallUP.sh done."
 
--- pkglist.sh
+++ pkglist.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #         pkglist.sh - list MANUALLY added pkgs (minus fw)
+#                      add -z to remove version-information
 #
 doas pkg_info -m|grep -v firmware
