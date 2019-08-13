7c812e4771009c9380d2f3361d2af25681de9500
hfeltonadmin <hfeltonadmin@hfelton.openbsd.amsterdam>
2019-08-13 22:33:12 +0200

    more mkmtrees.sh fixes

--- mkmtrees.sh
+++ mkmtrees.sh
@@ -1,35 +1,41 @@
 #!/bin/ksh
 #
-# hjf last mod: 2019-08-13 07:30 PDT
+# hjf last mod: 2019-08-13 13:30 PDT
 #
 # mkmtrees.sh
 #
 # create default mtrees for trojan-catching...
 
 export MKMTREEPATHS="/bin /sbin /usr/bin /usr/sbin"
-#export MATCHNAMES="bin sbin usr_bin usr_sbin"
 export FNDIR="/etc/mtree"
 export FNTYP="secure"
 export FNOWN="root:wheel"
 export FNMOD="600"
-export TODAY=`date "%+"`
+# final cksum cmd --AND-- intermediate digests
+export SHA256="sha256"
+# seconds since epoch-UTC
+export TODAY=`date +"%s"`
 export TMPLOC=`mktemp`
-export FINALCKSUM=`mktemp`
+export TMPNAMS=`mktemp`
+export TMPCKSUM=`mktemp`
 
 for ITEM in $MKMTREEPATHS; do
-	# remove / for filename
+	# remove-or-xlate / for filename
 	FN=`echo "$ITEM"|sed -e s:/:: -e s:/:_:g`
 	FNFULL="$FNDIR/$FN.$FNTYP"
-	echo "making mtree for $ITEM --> $FNFULL (seed: $TODAY)"
-	CMD="mtree -cix -p $ITEM -K sha256digest,type -s $TODAY"
-	doas $CMD > $TMPLOC 2> $FINALCKSUM
-	doas cp -fp $FINALCKSUM $FNDIR/seed_$TODAY.fullcksum
+	echo "$FNFULL" >> $TMPNAMS
+	echo "making mtree for $ITEM --> $FNFULL (epoch: $TODAY)"
+	CMD="mtree -cix -p $ITEM -K ${SHA256}digest,type "
+	doas $CMD > $TMPLOC 
 	doas cp -fp $TMPLOC $FNFULL
 	doas chown $FNOWN $FNFULL
 	doas chmod $FNMOD $FNFULL
 done
-# how to "fix" the mod-time on direc itself...
-#doas mtree -df /etc/mtree/bin.secure -t -p /bin/\.
-# afterwards, take digest-of-digest-file for offsite safekeeping...
-# before-wards, reset all dirs to read-only ?
-# howto set touch-fixups for changes from syspatch, etc...
+FNLIST=`cat $TMPNAMS`
+doas $SHA256 $FNLIST > $TMPCKSUM
+FINALCKSUM="$FNDIR/$SHA256_$TODAY.fullcksum"
+doas cp -fp $TMPCKSUM $FINALCKSUM
+doas chown $FNOWN $FINALCKSUM
+doas chmod 444 $FINALCKSUM
+#echo "$0 complete: master cksums stored at $FINALCKSUM"
+echo "mkmtrees.sh done... : master cksums stored at $FINALCKSUM"
