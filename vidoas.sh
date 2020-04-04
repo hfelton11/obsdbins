b5cc5d943299a90dbbac1ed62c4219c2573aaee9
Harold Felton <work@hfelton.com>
2020-04-04 09:41:13 -0700

    vidoas.sh update strings

--- vidoas.sh
+++ vidoas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# hjf latest mod: 2019-08-15 @ 07:30 PDT
+# hjf latest mod: 2020-04-04 @ 09:30 PDT
 #
 ## vidoas.sh
 #
@@ -14,12 +14,20 @@
 export DOASFILE="/etc/doas.conf"
 export TTY=`tty`
 export USR=`whoami`
-export TESTING_STRING="Currently 4 possible tests: \n \
+export VIDOAS=`basename $0`
+export END_STRING="$VIDOAS: succeeded."
+export DEBUG_STRING=" \
 1. incorrect doas-pw for initial copy... \n \
 2. incorrect edit of doas-file... \n \
 3. incorrect permissions of valid doas-file... \n \
 4. incorrect doas-pw for final replacement... \n"
+export TESTING_STRING="Currently 4 possible tests: \n$DEBUG_STRING "
 export DODEBUG=
+export DONORMAL=
+export DOHELPFUL=
+export START_STRING="$VIDOAS: MUST supply password before AND after edits..."
+export XPLAINING_STRING="Password is normally required twice... \n \
+ due to Steps 1. and 4. from .... \n$DEBUG_STRING"
 
 function setup { 
 	export LAUNCH_CMDS=`mktemp`
@@ -40,9 +48,13 @@ function assert {
 	printf "\nFAIL: $this\n'$1' != '$2'\n"; teardown; exit 1
 }
 
-try "0. TESTING..."
+# MAIN-STARTS-HERE (assuming setup;)
+#
+#try "0. TESTING..."
 [ "$DODEBUG" ] && { echo $TESTING_STRING; }
-assert "`echo 't'`" "t"
+[ "$DOHELPFUL" ] && { echo $XPLAINING_STRING; }
+[ "$DONORMAL" ] && { echo $START_STRING; }
+#assert "`echo 't'`" "t"
 
 try "1. create an edit-able copy..."
 cat > $LAUNCH_CMDS <<-'LAUNCHER'
@@ -69,7 +81,12 @@ assert "`doas -C $VI_FILE -u $USR cp | cut -c 1-6 `" "permit"
 try "4. install the latest-greatest back..."
 assert "`doas cp $VI_FILE $DOASFILE 2>&1`" ""
 
-##echo; echo "PASS: $tests_run tests run"
-echo "vidoas.sh succeeded."
+# MAIN-ENDS-HERE....
+#
+#try "999. Testing ENDS..."
+[ "$DODEBUG" ] && { echo; echo "PASS: $tests_run tests run"; }
+[ "$DONORMAL" ] && { echo $END_STRING; }
+#assert "`echo 't'`" "t"
+##echo "vidoas.sh succeeded."
 teardown; exit 0
 
