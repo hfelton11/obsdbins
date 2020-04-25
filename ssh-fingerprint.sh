18bb768631d55a8a5039ec6767093d0275415a32
Harold Felton <work@hfelton.com>
2020-04-25 03:04:10 -0700

    ssh-fingerprint.sh updates

--- ssh-fingerprint.sh
+++ ssh-fingerprint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-# this lists the www-host-keyfile-fingerprint...
+# the FULL list of POSSIBLE fingerprints is...
+ssh-keygen -l -f /etc/ssh/ssh_host_dsa_key.pub
+ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_dsa_key.pub
 ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub
+ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_ecdsa_key.pub
+ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub
+ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_ed25519_key.pub
+ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub
+ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_rsa_key.pub
